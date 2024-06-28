4595df782979a29b78d4869021f130743badfb73
Abdullah Mohammed <58551093+shovo4@users.noreply.github.com>
2024-06-28 05:08:03 -0600

    Individual Movie component, Movie List Component, Generic List Component

---
+++ moviemania-frontend/src/movies/MoviesList.tsx
@@ -0,0 +1,19 @@
+import IndividualMovie from "./IndividualMovie";
+import { movieDTO } from "./movies.model";
+import css from './MoviesList.module.css';
+import Loading from "../utils/Loading";
+import GenericList from "../utils/GenericList";
+
+export default function MoviesList(props: moviesListProps) {
+
+    return <GenericList list={props.movies}>
+            <div className={css.div}>
+                {props.movies?.map(movie => <IndividualMovie {...movie} key={movie.id} />)}
+            </div>
+    </GenericList>
+
+}
+
+interface moviesListProps {
+    movies?: movieDTO[];
+}
---
+++ moviemania-frontend/src/utils/Loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+  return (
+    <img alt="loading" src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" />
+  );
+}
